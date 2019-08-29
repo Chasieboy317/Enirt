cc115f19a3eab3ed4958eef9706a789ca2b47e9c
Brooke Stewart <brooke44@live.co.za>
2019-08-29 14:48:08 +0200

    Add destructable script

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public int health;
+
+    public void takeDamage(int d)
+    {
+        health -= d;
+
+        if (d < 0)
+        {
+            Debug.Log("Destroyed");
+            // Destroy object - might want to call specific script so that you can have particle effect
+        }
+    }
+}
