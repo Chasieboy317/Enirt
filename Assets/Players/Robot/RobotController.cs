c72e5beeca6ce21c646e40ec271be1d2bf48d486
root <root@DESKTOP-H3F6PEC.localdomain>
2019-09-02 11:30:58 +0200

    Fixed fucked merge

--- Assets/Players/Robot/RobotController.cs
+++ Assets/Players/Robot/RobotController.cs
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RobotController : PlayerController
-{
-    //Key codes for actions unique to knight
-    public KeyCode crawling;
-    public KeyCode aim;
-    public KeyCode shoot;
-    public KeyCode launchBomb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        OnStart(); //base class
-
-        aim = KeyCode.O;// KeyCode.Greater;
-        shoot = KeyCode.Mouse0; //left mouse
-        launchBomb = KeyCode.Mouse1; //right click
-        crawling = KeyCode.P;// KeyCode.Less; 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        OnUpdate(); //base class
-
-        /*
-         * SHOOT
-         */
-        if (Input.GetKey(crawling))
-        {
-            animController.SetBool("isCrawling", true);
-        }
-        else
-        {
-            animController.SetBool("isCrawling", false);
-        }
-
-        /*
-         * Blocking
-         */
-        if (Input.GetKey(aim))
-        {
-            animController.SetBool("isShooting", true);
-
-            //implement shooting and bomb launching
-        }
-        else
-        {
-            animController.SetBool("isShooting", false);
-        }
-    }
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -160,4 +103,3 @@ public class RobotController : PlayerController
         }
     }
 }
->>>>>>> CharacterAnimation
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraController : MonoBehaviour
-{
-    public float speed;
-    public float zoom;
-    public GameObject knight;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        /*
-         * TO DO:
-         * the zoom should be a percentage of the distance between the player and the camera
-         * so you should be able to do something like
-         * distanceFromPlayer = (camera.x-player.x)*zoom
-         */
-        //apply the correct amount of zoom
-        transform.position = knight.transform.position;
-        //transform.position += transform.forward * zoom;
- 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       // if (Input.GetKey(KeyCode.A))
-       // {
-       //     transform.position += -transform.right * speed * Time.deltaTime;
-       // }
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.position += transform.right * speed * Time.deltaTime;
-       // }
-
-        transform.position = new Vector3(4, 2.7f, knight.transform.position.z);
-    }
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -76,4 +32,3 @@ public class CameraController : MonoBehaviour
         }
     }
 }
->>>>>>> CharacterAnimation
