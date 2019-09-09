a35bb235de2dc5e04f9e68380959d40f6db21319
Brooke Stewart <brooke44@live.co.za>
2019-09-09 22:07:15 +0200

    spawning
    pressure plates spawn doppelgangers and then self destruct. Gem also appears

--- Assets/Scripts/level3ManagerScripts/DoppelgangerBattle.cs
+++ Assets/Scripts/level3ManagerScripts/DoppelgangerBattle.cs
@@ -4,55 +4,68 @@ using UnityEngine;
 
 public class DoppelgangerBattle : MonoBehaviour
 {
-    public PressurePlate ppLeft;
-    public PressurePlate ppRight;
+    public GameObject Gem;
+
+    PressurePlate ppLeft;
+    PressurePlate ppRight;
+
+    public GameObject PPLeft;
+    public GameObject PPRight;
 
     public Transform spawnPointLeft;
     public Transform spawnPointRight;
 
     public GameObject knightDoppelgangerPrefab;
     public GameObject robotDoppelgangerPrefab;
-    public GameObject spawn1;
-    public GameObject spawn2;
+    GameObject spawn1;
+    GameObject spawn2;
 
     public GameObject respawnEffect;
-    public float spawnTime;
-    public float spawnTimeTotal = 2.0f;
+    float spawnTime;
+    float spawnTimeTotal = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ppLeft = (PressurePlate) PPLeft.transform.gameObject.GetComponent("PressurePlate");
+        ppRight = (PressurePlate)PPRight.transform.gameObject.GetComponent("PressurePlate");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(ppLeft.triggered && ppRight.triggered) //spawn doppelgangers
+        if(PPLeft !=null && PPRight != null)
         {
-            RaycastHit player;
-            if(Physics.Raycast(ppLeft.transform.position, Vector3.up, out player))
+            if (ppLeft.triggered && ppRight.triggered) //spawn doppelgangers
             {
-                spawn1 = Instantiate(respawnEffect, spawnPointRight.position + new Vector3(0, 1, 0), spawnPointRight.rotation);
-                spawn2 = Instantiate(respawnEffect, spawnPointLeft.position + new Vector3(0, 1, 0), spawnPointLeft.rotation);
-                spawnTime = Time.time;
+                RaycastHit player;
+                if (Physics.Raycast(ppLeft.transform.position, Vector3.up, out player))
+                {
+                    spawn1 = Instantiate(respawnEffect, spawnPointRight.position + new Vector3(0, 1, 0), spawnPointRight.rotation);
+                    spawn2 = Instantiate(respawnEffect, spawnPointLeft.position + new Vector3(0, 1, 0), spawnPointLeft.rotation);
+                    spawnTime = Time.time;
 
-                if (player.transform.tag == "Knight")
-                {
-                    Instantiate(knightDoppelgangerPrefab, spawnPointRight.position, spawnPointRight.rotation);
-                    Instantiate(robotDoppelgangerPrefab, spawnPointLeft.position, spawnPointLeft.rotation);
+                    if (player.transform.tag == "Knight")
+                    {
+                        Instantiate(knightDoppelgangerPrefab, spawnPointRight.position, spawnPointRight.rotation);
+                        Instantiate(robotDoppelgangerPrefab, spawnPointLeft.position, spawnPointLeft.rotation);
+                    }
+                    else
+                    {
+                        Instantiate(robotDoppelgangerPrefab, spawnPointRight.position, spawnPointRight.rotation);
+                        Instantiate(knightDoppelgangerPrefab, spawnPointLeft.position, spawnPointLeft.rotation);
+                    }
+                    Destroy(PPLeft);
+                    Destroy(PPRight);
+
+                    Gem.SetActive(true);
                 }
-                else
-                {
-                    Instantiate(robotDoppelgangerPrefab, spawnPointRight.position, spawnPointRight.rotation);
-                    Instantiate(knightDoppelgangerPrefab, spawnPointLeft.position, spawnPointLeft.rotation);
-                }
-                Destroy(ppLeft);
-                Destroy(ppRight);
             }
         }
+        
         if (Time.time - spawnTime > spawnTimeTotal)
         {
+            Debug.Log("should destroy spawn effect");
             Destroy(spawn1);
             Destroy(spawn2);
         }
