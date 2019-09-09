27e538f573b3fc0dbd4a03f97a2c4816fdbabf53
Chase Abrahams <cabrahams317@gmail.com>
2019-09-09 16:51:45 +0200

    changed the name of CmaeraController to CameraFollow, but the functionality is still the same

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+
+    private float deltaPos;
+    private float lastPos;
+    void Start()
+    {
+        lastPos = player.transform.position.z;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        deltaPos = player.transform.position.z - lastPos;
+        Debug.Log("Delta is now: " + deltaPos);
+        transform.position += new Vector3(0.0f, 0.0f, deltaPos);
+        lastPos = transform.position.z;
+    }
+}
