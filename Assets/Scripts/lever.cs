8183a79ec0a6b9b4f445054dd7d2a31e9f87b397
Chase Abrahams <cabrahams317@gmail.com>
2019-09-12 23:38:40 +0200

    implemented rotation on level script, optimized code

--- Assets/Scripts/lever.cs
+++ Assets/Scripts/lever.cs
@@ -5,23 +5,62 @@ using UnityEngine;
 public class lever : MonoBehaviour
 {
     public bool activated;
+    public Vector3 pivot;
+    public Vector3 direction;
+    public float maxAngle;
+    public float speed;
+
+    private float currentTime;
+    private float waitTime;
+    private bool cycle;
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        waitTime = 1.0f / speed;
+        currentTime = 0.0f;
         activated = false;
+        cycle = true;
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        move();
     }
 
     // Update is called once per frame
-    void activate()
+    void toggle()
     {
-        if (!activated)
+        activated = activated ? false : true;
+        cycle = true;
+        currentTime = 0.0f;
+        transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+        transform.position = startPos;    
+    }
+
+    void move()
+    {
+        if (activated)
         {
-            activated = true;
+            currentTime += Time.deltaTime;
+            if (cycle)
+            {
+                var rotation = transform.rotation;
+                transform.RotateAround(pivot, direction, maxAngle * speed * Time.deltaTime);  
+                transform.rotation = rotation;
+                transform.Rotate(direction * maxAngle * speed * Time.deltaTime);
+                if (currentTime>=waitTime) { cycle = false; currentTime = 0.0f; }
+            }
+            else
+            {
+                var rotation = transform.rotation;
+                transform.RotateAround(pivot, direction, -maxAngle * speed * Time.deltaTime);
+                transform.rotation = rotation;
+                transform.Rotate(direction * -maxAngle * speed * Time.deltaTime);
+                if (currentTime>=waitTime) { toggle(); }
+            }
         }
-        else
-        {
-            activated = false;
-        }
-        
     }
 }
