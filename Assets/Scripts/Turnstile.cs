a4dc9395d89c57d7691968a1a407965698e5f16f
Brooke Stewart <brooke44@live.co.za>
2019-09-12 13:32:52 +0200

    Level 3 Turnstile
    turnstile works (normal pushing unaffected)
    also added script to manage all of level 3

---
+++ Assets/Scripts/Turnstile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turnstile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerspushing;
+    public Transform checkPoint;
+    public bool turned;
+    void Start()
+    {
+        playerspushing = 0;
+        turned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!turned && playerspushing == 4)
+        {
+            if (Physics.Raycast(checkPoint.transform.position, Vector3.up))
+            {
+                transform.Rotate(new Vector3(0, -45, 0) * Time.deltaTime * 0.1f);
+                turned = true;
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime * 0.1f);
+                turned = true;
+            }
+        }
+        playerspushing = 0;
+    }
+
+    void playerPushing(int num)
+    {
+        playerspushing += num;
+    }
+}
---
+++ Assets/Scripts/level3ManagerScripts/Level3Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3Manager : MonoBehaviour
+{
+    public bool gameOver;
+    
+
+    //start timer?
+    //win/lose conditions
+
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            //end game //check win or loose
+            Debug.Log("GameOver");
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+    }
+}
