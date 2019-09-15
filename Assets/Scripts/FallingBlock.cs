3cb0aba2e92917d9eb791f4c00c88030b9a9a723
Brooke Stewart <brooke44@live.co.za>
2019-09-15 15:52:01 +0200

    fallingBlocksScript

---
+++ Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public float hangTime = 1f;
+    public float collisionTime;
+    public int collisions;
+
+    void Update()
+    {
+        if (collisions > 1)
+        {
+           
+        }
+
+        if (collisions>=1 && Time.time > collisionTime + hangTime)
+        {
+
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        collisions++;
+
+    }
+}
