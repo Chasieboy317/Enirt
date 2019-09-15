3079b71e425251930a8cacbe243fc8d3eddc19e5
Brooke Stewart <brooke44@live.co.za>
2019-09-15 13:31:32 +0200

    Level 3 enemies spawn and target doppelgangers

--- Assets/Scripts/level3ManagerScripts/Level3Manager.cs
+++ Assets/Scripts/level3ManagerScripts/Level3Manager.cs
@@ -7,17 +7,39 @@ public class Level3Manager : MonoBehaviour
     public bool gameOver;
     public bool successfulCompletion;
 
+    //players
+    public GameObject KNIGHT;
+    public GameObject ROBOT;
+
+    //For final battle
+    public bool finalBattle;
+    public GameObject doppelgangerBattle;
+    public Transform finalBattleSpawnLeft;
+    public Transform finalBattleSpawnRight;
+
     //start timer?
     //win/lose conditions
 
     void Start()
     {
+        finalBattle = false;
         gameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //when Time for final battle - activate level and teleport players there
+        if (finalBattle)
+        {
+            doppelgangerBattle.SetActive(true);
+            KNIGHT.transform.position = finalBattleSpawnLeft.position;
+            KNIGHT.transform.rotation = finalBattleSpawnLeft.rotation;
+            ROBOT.transform.position = finalBattleSpawnRight.position;
+            ROBOT.transform.rotation = finalBattleSpawnRight.rotation;
+        }
+        
+
         if (gameOver)
         {
             //end game //check win or loose
