d5d7ea7494f961802715d24bdb3b77be58b61220
Chase Abrahams <cabrahams317@gmail.com>
2019-09-17 00:20:24 +0200

    level1 pretty much done. Added another camera to allow the player to see the blocks which must be moved up in puzzle4. Added a loadlevel script that will load the selected level only if both players go through the trigger

---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public GameObject triggeredController;
+    public GameObject cameraController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggeredController.SetActive(false);  
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Knight"||other.gameObject.tag=="Robot")
+        {
+            triggeredController.SetActive(true);
+            cameraController.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Knight" || other.gameObject.tag == "Robot")
+        {
+            triggeredController.SetActive(false);
+            cameraController.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/MainCameraFollow.cs
+++ Assets/Scripts/MainCameraFollow.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class MainCameraFollow : MonoBehaviour
 {
+    public Vector3 offset;
     private GameObject robot;
     private GameObject knight;
 
-    private Vector2 lastPos;
-    private Vector2 deltaPos;
-    // Start is called before the first frame update
     void Start()
     {
         robot = GameObject.FindWithTag("Robot");
         knight = GameObject.FindWithTag("Knight");
-
-        lastPos = new Vector2((robot.transform.position.y+knight.transform.position.y)/2,
-                                (robot.transform.position.z + knight.transform.position.z)/2);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        deltaPos = new Vector2((robot.transform.position.y + knight.transform.position.y) / 2 - lastPos.x,
-                                (robot.transform.position.z + knight.transform.position.z) / 2 - lastPos.y);
-        transform.position += new Vector3(0.0f, deltaPos.x, deltaPos.y);
-        lastPos = new Vector2((robot.transform.position.y + knight.transform.position.y) / 2,
-                                (robot.transform.position.z + knight.transform.position.z) / 2);
+        transform.position = new Vector3(transform.position.x, (robot.transform.position.y + knight.transform.position.y) / 2,
+            (robot.transform.position.z + knight.transform.position.z) / 2) + offset;
     }
 }
