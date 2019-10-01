6a641a90bf5b0e5ef3e97cbd1c8e79ec735b3315
Chase Abrahams <cabrahams317@gmail.com>
2019-10-01 20:49:54 +0200

    created pause menu with full functionality

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool toggle;
+    private GameObject pauseMenu;
+    void Start()
+    {
+        pauseMenu = GameObject.FindWithTag("PauseMenu");
+        pauseMenu.SetActive(false);
+        toggle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!toggle) pauseMenu.SetActive(true);
+            else pauseMenu.SetActive(false);
+            toggle = !toggle;
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
