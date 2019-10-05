580d380bb7bdb12af1ef842f77b39eaafc8c9588
Brooke Stewart <brooke44@live.co.za>
2019-10-05 13:04:43 +0200

    added pressure plate sound effect

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -8,10 +8,13 @@ public class PressurePlate : MonoBehaviour
     public GameObject triggerEntity; // Object that is triggering the pressure plate e.g. player
     public float downHeight = 0.1f; // How far a pressureplate sinks when activated
 
+    public AudioSource audioSource;
+
     void Start()
 
     {
         triggered = false; //this value refers to whether only a single pressure plate is triggered
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,6 +22,7 @@ public class PressurePlate : MonoBehaviour
         triggerEntity = other.gameObject; // Set the object which is triggering the pressureplate
         if (!triggered)
         {
+            audioSource.Play();
             triggered = true;
             this.transform.position += new Vector3(0.0f, -downHeight, 0.0f); //move the pressure plate down to reflect the change
             Debug.Log("OnTriggerEnter " + triggered);
@@ -27,6 +31,7 @@ public class PressurePlate : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        audioSource.Play();
         triggered = false;
         triggerEntity = null;
         this.transform.position += new Vector3(0.0f, downHeight, 0.0f); //move the pressure plate back up
