5101189117c08359efd9fe99997e8d348a9ca3c3
Brooke Stewart <brooke44@live.co.za>
2019-10-07 11:18:01 +0200

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offest;
+    public Vector3 offset;
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z) + offest;
+        transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z) + offset;
     }
 }
