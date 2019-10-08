8dd367c15ba14effc689665d79b43c82a49f5c52
Brooke Stewart <brooke44@live.co.za>
2019-10-08 23:39:42 +0200

    more comments

--- Assets/Scripts/Interactable/Destructable.cs
+++ Assets/Scripts/Interactable/Destructable.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * Attach to any object you wish to be destructable
+ * Player and enemy weapons send messages to this script and so can destroy objects with this script 
+ * health can be set in the inspector
+ */
 public class Destructable : MonoBehaviour
 {
     public int health;
@@ -11,16 +16,16 @@ public class Destructable : MonoBehaviour
     {
         health -= d;
 
-        if (health < 0)
+        if (health <= 0)
         {
-            //play destruction effect //haven't tested this yet
+            //play destruction effect if it exists
             if (this.transform.gameObject.GetComponent<ParticleSystem>())
             {
                 this.transform.gameObject.GetComponent<ParticleSystem>().Play();
             }
             Destroy(this.gameObject);
-            Debug.Log("Destroyed");
-            if (transform.gameObject.tag == "Knight" || transform.gameObject.tag == "Robot") SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //Debug.Log("Destroyed");
+            //if (transform.gameObject.tag == "Knight" || transform.gameObject.tag == "Robot") SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         else if (health>10)
         {
