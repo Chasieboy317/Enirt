ded0a0d674ba96e89d10dae7115f2b4e064805c2
Alec Badenhorst <badenhorst.alec@gmail.com>
2019-10-09 11:18:24 +0200

    Updated timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,6 +8,7 @@ public class Timer : MonoBehaviour
     public float totalTime;
     private float currentTime;
     private UnityEngine.UI.Text text;
+    private int minutes = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +19,24 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = ""+Mathf.Round(totalTime - currentTime);
+        //text.text = ""+Mathf.Round(totalTime - currentTime);
+        text.text = calcMinutes(currentTime);
         currentTime += Time.deltaTime;
         if (currentTime >= totalTime)
         {
             Debug.Log("end");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }     
+    }
+
+    string calcMinutes(float currentTime) {
+        int minutes = 0;
+        string result;
+        while (currentTime%60==0) {
+            currentTime-=60;
+            minutes++;
+        }
+        result = minutes + ":" + currentTIme;
+        return result;
     }
 }
